ea5f40f4451d38afa2154c512ed7c0eea5dde7da
ZRE <chy853@gmail.com>
2025-03-18 15:41:02 +0800

    rewrite

--- HW1/FileCopy.c
+++ HW1/FileCopy.c
@@ -3,42 +3,33 @@
 #include <stdio.h>
 #include <sys/stat.h>
 
-int main(int argc, char *argv[]) {
-    struct stat statbuf;
-    if (argc != 3) {
-        printf("Usage: %s <source> <destination>\n", argv[0]);
-        return 1;
-    }
-    if (stat(argv[1], &statbuf) < 0) {
-        perror("stat");
-        return 1;
-    }
-    if (S_ISDIR(statbuf.st_mode)) {
-        printf("Cannot copy a directory\n");
-        return 1;
-    }
-    FILE *src = fopen(argv[1], "r");
-    if (src == NULL) {
-        perror("fopen");
-        return 1;
-    }
-    FILE *dst = fopen(argv[2], "w");
-    if (dst == NULL) {
-        perror("fopen");
-        fclose(src);
-        return 1;
-    }
-    char buffer[4096];
-    size_t n;
-    while ((n = fread(buffer, 1, sizeof(buffer), src)) > 0) {
-        if (fwrite(buffer, 1, n, dst) != n) {
-            perror("fwrite");
-            fclose(src);
-            fclose(dst);
-            return 1;
-        }
-    }
-    fclose(src);
-    fclose(dst);
-    return 0;
+int main(int argc, char **argv) {
+  if (argc != 3) {
+    printf("Usage: %s <source> <destination>\n", argv[0]);
+    return 1;
+  }
+  struct stat statbuf;
+  if (stat(argv[1], &statbuf) == 0 && S_ISDIR(statbuf.st_mode)) {
+    printf("Cannot copy a directory\n");
+    return 1;
+  }
+  FILE *fp1;
+  fp1 = fopen(argv[1], "r");
+  if (fp1 == NULL) {
+    printf("Cannot open file %s\n", argv[1]);
+    return 1;
+  }
+  FILE *fp2;
+  fp2 = fopen(argv[2], "w");
+  if (fp2 == NULL) {
+    printf("Cannot open file %s\n", argv[2]);
+    return 1;
+  }
+  ssize_t c;
+  while ((c = fgetc(fp1)) != EOF) {
+    fputc(c, fp2);
+  }
+  fclose(fp1);
+  fclose(fp2);
+  return 0;
 }
